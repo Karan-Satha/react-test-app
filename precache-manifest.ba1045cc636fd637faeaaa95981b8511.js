9804662edea17333891496d91f7fcaa1c9e2fbad
Jamunatharan <jamunatharan@gmail.com>
2020-07-27 14:31:14 +0100

    Updates

---
+++ precache-manifest.ba1045cc636fd637faeaaa95981b8511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1f9fb90a38e32c0eaa5d0fda4c29f9",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "644be632cc9658d12354",
+    "url": "/react-test-app/static/css/main.9cc1dd63.chunk.css"
+  },
+  {
+    "revision": "de5fd8c162aced8fe422",
+    "url": "/react-test-app/static/js/2.8783f5af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test-app/static/js/2.8783f5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644be632cc9658d12354",
+    "url": "/react-test-app/static/js/main.b77be1d6.chunk.js"
+  },
+  {
+    "revision": "754588e88452220c8878",
+    "url": "/react-test-app/static/js/runtime-main.d40c1bd5.js"
+  }
+]);
